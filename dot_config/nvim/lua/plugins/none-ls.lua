0ffb1dacad3d2e80619438fa1bc50edb866d20dd
Aluisio Amaral <aluisio@m.io>
2024-03-08 18:08:39 -0300

    added eslint and prettier

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -5,8 +5,12 @@ return {
 		none_ls.setup({
 			sources = {
 				none_ls.builtins.formatting.stylua,
+				-- PHP
 				none_ls.builtins.formatting.phpcsfixer,
 				none_ls.builtins.diagnostics.phpstan,
+				-- Javacript
+				none_ls.builtins.formatting.prettier,
+				none_ls.builtins.diagnostics.eslint,
 			},
 		})
     require("keymaps").none_ls()
