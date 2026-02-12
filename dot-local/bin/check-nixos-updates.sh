541fb6b32ae4ac57456da6a0d58af4e3e13e9ca2
Aluisio Amaral <aluisioramtl@gmail.com>
2026-02-12 18:26:55 -0300

    chore(nix): updated flake

--- dot-local/bin/check-nixos-updates.sh
+++ dot-local/bin/check-nixos-updates.sh
@@ -13,10 +13,13 @@ nix flake update --quiet 2>/dev/null
 # Compare the current system to the "new" evaluation
 # We use 'nix store diff-closures' to see the changed paths
 # We grep for '+' to count only new/upgraded packages
-UPDATES_COUNT=$(nix store diff-closures /run/current-system .#nixosConfigurations."$(hostname)".config.system.build.toplevel 2>/dev/null | grep -c '^+')
+NIX_UPDATES=$(nix store diff-closures /run/current-system .#nixosConfigurations."$(hostname)".config.system.build.toplevel 2>/dev/null | grep -c '^+')
+
+FLATPAK_UPDATES=$(flatpak remote-ls --updates --columns=application | wc -l)
 
 # Output the raw number
-echo "$UPDATES_COUNT"
+TOTAL_UPDATES=$((NIX_UPDATES + FLATPAK_UPDATES))
+echo "$TOTAL_UPDATES" | xargs
 
 # Cleanup
 rm -rf "$TEMP_DIR"
--- dot-local/bin/nixos-update.sh
+++ dot-local/bin/nixos-update.sh
@@ -2,6 +2,14 @@
 
 set -e
 
-cd ${HOME}/.config/nix
-nix flake update
-sudo nixos-rebuild switch --flake .
+flatpak update -y --noninteractive
+
+# NixOS update
+NIXOS_UPDATES=$(${HOME}/.local/bin/check-nixos-updates.sh)
+
+if (( $NIXOS_UPDATES > 0 )); then
+  cd ${HOME}/.config/nix
+  nix flake update
+  sudo nixos-rebuild switch --flake .
+fi
+
